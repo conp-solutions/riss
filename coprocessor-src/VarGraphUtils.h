e0f274a54642dd749bc94c44832111c3c3f8a435
Tobias Nett <skaldarnar@googlemail.com>
2012-11-16 22:13:34 +0100

    some further implementation of VarGraphUtils.

--- coprocessor-src/VarGraphUtils.h
+++ coprocessor-src/VarGraphUtils.h
@@ -25,6 +25,7 @@ class VarGraphUtils
 inline void partVars(int noParts, std::vector<std::vector<Var> >& variables, int bufferSize)
 {
   //TODO: find good starting points
+  //TODO: use sets instead of vectors - adding only unique elements, find() method, ... ?
   std::vector<std::vector<Var> > buffers;    // buffer vectors for breadth-first-search
   std::vector<std::vector<Var> > finalVars;  // vectors containing partitioned variables
 
@@ -33,9 +34,13 @@ inline void partVars(int noParts, std::vector<std::vector<Var> >& variables, int
     locked.nextStep();
 
   bool finished = false;
+
+  // ---------- initialize buffers --------------
+
+  // --------- end initialize buffers -----------
   while(!finished)
   {
-  std::vector<Var> & buffer;
+  std::vector<Var> buffer;
     for (int i = 0; i < noParts; ++i)
     {
       // do breadth-first-search-step for every starting point
@@ -44,26 +49,53 @@ inline void partVars(int noParts, std::vector<std::vector<Var> >& variables, int
       {
         Var & active = buffer[j];
         // find all succesors for _active_ -> mark1()
-
-        // chech local
-
-        // check global
-
-        // update buffer and var array and locks
+        MarkArray array;
+          array.create(solver->nVars());
+          array.nextStep();
+        CoprocessorData::mark1(active, array);  // all marked variables are connected to the active node
+        int k;
+        for (k = 0; k < array.size(); ++k)
+        {
+          if (array.isCurrentStep(k))   // _k_ is successor
+          {
+            // TODO: deal with _active_ being successor of itself
+            if (locked.isCurrentStep(k) ) { // k is already locked
+              // check if is owned by this search instance
+              if ( std::find(buffer.begin(), buffer.end(), k) == buffer.end() ) {
+                break;
+              }
+            } else {
+              locked.setCurrentStep(k);   // lock successor variable for further actions
+              buffer.push_back(k);  // push sucessore into the buffer
+            }
+          }
+        }
+        // if no successor clashed with locked variables, we can add the active var to tha final var list
+        if (k == array.size())  // this means that the was no lock clash
+          finalVars[i].push_back(active);
       }
     }
     // test if all buffers are empty -> finished!
+    finished = true;
+    for (std::vector<Var>& v : buffers)
+    {
+      if (!v.empty())
+        finished = false;
+    }
   }
+  // after this while loop, the final vectors contain the partitions.
 }
 
 inline void partClauses(std::vector<std::vector<Clause> >& variables, int bufferSize)
 {
-
+  // TODO: implement partitioning for clauses
 }
 
 inline void sortVars(std::vector<Var>& variables)
 {
-
+  // TODO: sort a vector of variables according to partitions
+  // TODO: write start and and position of each partition into a separate vector
+  // TODO: pass another argument for number of partitions?
 }
 }
 
