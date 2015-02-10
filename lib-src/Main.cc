e092438d7594e833f9f4d41969f03d406dd3e097
Norbert Manthey <Norbert.Manthey@tu-dresden.de>
2015-02-10 23:04:34 +0100

    add libcheck built target

---
+++ lib-src/Main.cc
@@ -0,0 +1,28 @@
+/*****************************************************************************************[Main.cc]
+Copyright (c) 2013, Norbert Manthey, All rights reserved.
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**************************************************************************************************/
+
+#include "VERSION" // include the file that defines the solver version
+
+#include "ipasir.h"
+#include "libcoprocessorc.h"
+#include "libprissc.h"
+#include "librissc.h"
+#include "NuSVM_SolverInterface.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  std::cerr << "c check implemented solver interfaces ... " << std::endl;
+  return 0;  
+}
