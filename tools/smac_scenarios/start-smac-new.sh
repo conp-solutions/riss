55ed5e05493ded48b264b917f872f66c0a22fd4c
Franziska Krueger <franziska.krueger1@tu-dresden.de>
2015-02-12 13:29:34 +0100

    Deletes wallclock-limit parameter

--- tools/smac_scenarios/start-smac-new.sh
+++ tools/smac_scenarios/start-smac-new.sh
@@ -6,27 +6,25 @@
 # start a slurm job for a SMAC scenario
 # usage:
 #        sbatch ./start-smac.sh <scenario dir> <wallclock limit (just local)>
-#        ./start-smac.sh MiniSATi scenario_name trail_file test_file [30] [ROAR
-#    TODO: fill complete list
+#        ./start-smac.sh MiniSATi scenario_name trail_file test_file [additional smac-parameter]
 # 
 # example call if the smac_scenarios folder is placed within the smac folder:
-#        sbatch ./start-smac.sh MiniSATi scenario_name trail_file test_file       -- with slurm
-#        ./start-smac.sh MiniSATi scenario_name trail_file test_file [30] [ROAR ...  -- without slurm
+#        sbatch ./start-smac.sh MiniSATi minisat-scenario.txt instances-train.txt instances-test.txt       -- with slurm
+#        ./start-smac.sh MiniSATi minisat-scenario.txt instances-train.txt instances-test.txt [--wallclock-limit 30] [--cutoff-time 10] [--executionMoade ROAR] ... -- without slurm
 #
 
 solver_dir=$1           # name of the solver that should be executed
 scenario_name=$2        # name of the scenario file (located in "scenarios" directory)
 train_instance_path=$3  # list of training files (relative path from here, or absolute path) 
 test_instance_path=$4   # list of test/validation files (relative path from here, or absolute path)
-wallclock_limit=$5      # timeout for local execution
 
-shift; shift; shift; shift; shift;
+shift; shift; shift; shift;
 
 usage="
 Usage as slurm-job:
-    sbatch --exclusive ./start-smac.sh <smac-folder> <scenario name>
+    sbatch --exclusive ./start-smac.sh <solver-dir> <scenario name> <train-instances-file> <test-instances-file>
 Usage as local:
-    ./start-smac.sh <smac-folder> <scenario name> <wallclock limit (just local)>"
+    ./start-smac.sh <solver-dir> <scenario name> <train-instances-file> <test-instances-file> <--additional smac-parameter>"
 
 if [ -z "$solver_dir" ]
 then
@@ -49,8 +47,7 @@ path=$PWD/solvers/$solver_dir
 additional_options="$*"
 
 # execute as slurm job on taurus
-if [ -z "$wallclock_limit" ]
-then
+
 # if the time for the batch job is changed, change tunertimeout below too
 #SBATCH --time=72:00:00           # walltime
 #SBATCH --ntasks=1                # number of processor cores
@@ -58,34 +55,25 @@ then
 #SBATCH --partition=sandy,west    # smp, sandy, west, and gpu available 
 #SBATCH -A p_sat                  # charge resources to specified project
 
-    # load necessary modules on taurus
-    module unload java; module unload python
-    module load java/jdk1.7.0_25; module load python/2.7
+# load necessary modules on taurus
+module unload java; module unload python
+module load java/jdk1.7.0_25; module load python/2.7
     
-    # this will be the folder name for smac_output of this smac run
+# this will be the folder name for smac_output of this smac run
+if [ -n "$SLURM_JOB_ID" ]; then
     additional_options="--rungroup job_$SLURM_JOB_ID"
-
 else
     echo "------Local run of SMAC with example CNFs---------"
     echo "The output can be found in: [scenarrio-dir]/smac_output/local-$(date +%s)"
-
-    # use example CNFs for a local run
-    # do the same as on the cluster!
-    # train_instance_path=$PWD/example_instances/instances-train.txt
-    # test_instance_path=$PWD/example_instances/instances-test.txt
 
     # set local output directory
     additional_options="$additional_options --rungroup local_$(date +%s)"
 fi
 
-# start smac, smac is located here
-# cd $smac_path
-
 # use optimization mode ( executionMode = SMAC) - ROAR is good to find bugs, but does not optimize
 
-echo "call: \
-./configurators/smac-v2.06.00-master-615/smac \
---numRun 0 \
+./smac \
+--execDir . \
 --executionMode SMAC \
 --outdir smac_output \
 --pcs-file $path/params.pcs \
@@ -95,7 +83,7 @@ echo "call: \
 --test_instance_file $test_instance_path \
 --tunerTimeout 252000 \
 --validation false \
-$additional_options"
+$additional_options
 
 #disabled options:
 # we are starting the script in the actual smac directory
