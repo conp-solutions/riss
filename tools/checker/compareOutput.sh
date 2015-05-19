c75f50e5da8db4c533e1ef449eeada676d4dd3d3
Norbert Manthey <Norbert.Manthey@tu-dresden.de>
2015-05-19 11:20:57 +0200

    add output compare script

---
+++ tools/checker/compareOutput.sh
@@ -0,0 +1,49 @@
+#!/bin/bash 
+
+params="-quiet -printDec=3 -no-learn-debug -verb=0"
+
+#
+# run the  solvers
+#
+./riss-master $params $1 2> /tmp/err_$$  | grep -v "time" | grep -v "riss" | grep -v "Ratios" | grep -v "cpu" | grep -v "sec" > /tmp/out_$$
+
+./riss-hacks  $params $1 2> /tmp/err2_$$ | grep -v "time" | grep -v "riss" | grep -v "Ratios" | grep -v "cpu" | grep -v "sec" > /tmp/out2_$$
+
+# stderr
+diff /tmp/err_$$ /tmp/err2_$$ #> /dev/null
+ec2=$?
+
+echo ""
+echo "STDERR1"
+echo ""
+cat /tmp/err_$$
+
+echo ""
+echo "STDERR2"
+echo ""
+cat /tmp/err2_$$
+
+echo ""
+echo ""
+echo ""
+
+# stdout
+diff /tmp/out_$$ /tmp/out2_$$ #> /dev/null
+ec1=$?
+
+# stderr missmatch
+if [ "$ec2" == "1" ]
+then
+  # analyze manually
+	meld /tmp/err_$$ /tmp/err2_$$
+	exit 2
+fi
+
+# stdout missmatch
+if [ "$ec1" == "1" ]
+then
+	exit 1
+fi
+
+
+exit 0
