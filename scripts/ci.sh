32ccc91fff5658dc80d9725b36a2257ea02f20ef
Franziska Krueger <kruegerfr@gmail.com>
2015-06-11 10:26:06 +0200

    Changes path in the script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,26 +6,28 @@
 # exits the script immediately if a command exits with a non-zero status
 set -e
 
-# directory of this script
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# get absolute path for root directory of this repository.
+# directory of this script (in repo/scripts)
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+repo="$(dirname "$script_dir")"
 
 echo "Build all targets"
-# rm -r build # TODO: remove this line
-# mkdir build
-# cd build
-# cmake -DCMAKE_BUILD_TYPE=Debug ..
-# make all
+cd $repo
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make all
 
 echo "Make fuzzer"
-cd $DIR/tools/checker
+cd $repo/tools/checker
 make
 
-echo "Run fuzzer on Riss"
 n=10
-./fuzzcheck.sh $DIR/build/bin/riss $n  >/dev/null 2>/dev/null
+echo "Run fuzzer on Riss"
+./fuzzcheck.sh $repo/build/bin/riss $n  >/dev/null 2>/dev/null
 
 echo "Run fuzzer on Pcasso"
-./fuzzcheck.sh $DIR/build/bin/pcasso $n  >/dev/null 2>/dev/null
+./fuzzcheck.sh $repo/build/bin/pcasso $n  >/dev/null 2>/dev/null
 
 echo "Run fuzzer on Pfolio"
-./fuzzcheck.sh $DIR/build/bin/pfolio $n  >/dev/null 2>/dev/null
+./fuzzcheck.sh $repo/build/bin/pfolio $n  >/dev/null 2>/dev/null
