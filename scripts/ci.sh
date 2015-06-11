d773052e87c7f8712b80bc061c20823cc8626598
Franziska Krueger <kruegerfr@gmail.com>
2015-06-11 11:30:33 +0200

    Adds some more comments

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 #
 # This script builds all targets and runs the fuzzer on the solvers. 
-# 
+#
+# It is intended to be used as a script for a CI-runner which will provide a
+# clear git repository where this script will be executed.
 
 # exits the script immediately if a command exits with a non-zero status
 set -e
 
-# get absolute path for root directory of this repository.
+# solvers for fuzzchecking
+solver=('riss' 'pcasso' 'pfolio')
+
 # directory of this script (in repo/scripts)
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-repo="$(dirname "$script_dir")"
+# get absolute path for root directory of the repository
+repo_dir="$(dirname "$script_dir")"
 
 echo "Build all targets"
-cd $repo
+cd $repo_dir
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make all
 
 echo "Make fuzzer"
-cd $repo/tools/checker
+cd $repo_dir/tools/checker
 make
 
 echo "Start fuzzchecking"
-# solvers for fuzzchecking
-solver=('riss' 'pcasso' 'pfolio')
-n_runs=10
+n_runs=100
 
 # check each solver
 for s in "${solver[@]}"
@@ -33,7 +36,7 @@ do
     echo "Run fuzzer with $n_runs cnfs on $s"
     
     # run fuzzcheck as background process and wait for it
-    ./fuzzcheck.sh $repo/build/bin/$s $n_runs > /dev/null 2> /dev/null &
+    ./fuzzcheck.sh $repo_dir/build/bin/$s $n_runs > /dev/null 2> /dev/null &
     wait $!
 
     # count lines of log for this run (in this file the seed of the bugs can be found)
