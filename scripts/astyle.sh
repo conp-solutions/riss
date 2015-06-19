ffb5926e49a9ae4c761680293d57eed6ff7dfc0b
Lucas Kahlert <lucas.kahlert@tu-dresden.de>
2015-06-19 13:17:04 +0200

    Formats all files found in the given directory with astyle.sh scripts

--- scripts/astyle.sh
+++ scripts/astyle.sh
@@ -10,23 +10,6 @@ else
     root="$1"
 fi
 
-folders="
-$root/aiger/
-$root/classifier/
-$root/cmake/
-$root/coprocessor/
-$root/mprocessor/
-$root/pcasso/
-$root/pfolio/
-$root/proofcheck/
-$root/qprocessor/
-$root/riss/
-$root/risslibcheck/
-$root/shiftbmc/
-$root/test/
-$root/tools/
-"
-
-find $root$folders \( -name '*.cc' -or -name '*.c' -or -name '*.h' \) -exec astyle --options=.astylerc {} \;
+find $root \( -name '*.cc' -or -name '*.c' -or -name '*.h' \) -exec astyle --options=.astylerc {} \;
 # clean up backup files
-find $root$folders \( -name '*.cc.orig' -or -name '*.c.orig' -or -name '*.h.orig' \) -exec rm -v {} \;
+find $root \( -name '*.cc.orig' -or -name '*.c.orig' -or -name '*.h.orig' \) -exec rm -v {} \;
