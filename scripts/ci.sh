4f754f6859ef9b53a2537706e414e1199137b74b
Franziska Krueger <Franziska.Krueger1@tu-dresden.de>
2015-07-12 19:49:18 +0200

    Corrects path for solver call

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #
-# This script builds all targets and runs the fuzzer on the solvers. 
+# This script builds all targets and runs the fuzzer on the solvers. Therefore it
+# creates the build folder for Debug-builds and the release folder for Release-builds.
 #
-# It is intended to be used as a script for a CI-runner which will provide a
-# clear git repository where this script will be executed.
+# It is intended to be used as a script for a CI-runner but can be used for local runs too.
 
 # exits the script immediately if a command exits with a non-zero status
 set -e
 
-# solvers for fuzzchecking
+# solvers with their specific parameters
 solver=('riss-simp' 'riss-core -config=Riss427:BMC_FULL' 'riss-core -config=CSSC2014' 'pfolio' 'pcasso -model -threads=2')
+
+# parameters that are the same for all solvers
 params="-mem-lim=2048"
 
 # directory of this script (in repo/scripts)
@@ -43,10 +45,12 @@ do
     # different build folder for debug build and release build
     # splitting the two builds speeds up local building
     if [ "$build_type" == "Release" ]; then
-        cd $repo_dir/release
+        build_folder="$repo_dir/release"
     else
-        cd $repo_dir/build
+        build_folder="$repo_dir/build"
     fi
+
+    cd $build_folder
 
     # make all
     echo "$build_type build"
@@ -66,7 +70,7 @@ do
         echo "Run fuzzer with $n_runs cnfs on $s"
 
         # write wrapper script to call solver with parameters
-        echo "$repo_dir/build/bin/$s $params \$1" > solver-wrapper.sh
+        echo "$build_folder/bin/$s $params \$1" > solver-wrapper.sh
         echo "exit \$?" >> solver-wrapper.sh
         chmod +x solver-wrapper.sh
         
