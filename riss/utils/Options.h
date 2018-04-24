dcd3d3108199b9bd3596077bec794c685a6c9a26
Norbert Manthey <nmanthey@conp-solutions.com>
2018-04-24 23:52:35 +0200

    riss: fix brackets
    This fixes some clang compilation warnings.

--- riss/utils/Options.h
+++ riss/utils/Options.h
@@ -80,7 +80,7 @@ class Option
         bool operator()(const Option* x, const Option* y)
         {
             int test1 = strcmp(x->category, y->category);
-            return test1 < 0 || test1 == 0 && strcmp(x->type_name, y->type_name) < 0;
+            return test1 < 0 || (test1 == 0 && strcmp(x->type_name, y->type_name) < 0);
         }
     };
 
